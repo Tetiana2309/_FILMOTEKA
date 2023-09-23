3f79fd76999a1b4f4e43a441f9e7eaafe7838eb4
Tetiana2309 <126595700+Tetiana2309@users.noreply.github.com>
2023-09-23 07:04:24 +0200

    ok
    ok

--- src/js/modalMovieMarkup.js
+++ src/js/modalMovieMarkup.js
@@ -1,31 +1,4 @@
-// api.js
-import { IMG_BASE_URL, IMG_W400 } from './api';
 
-const saveLs = (key, value) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    console.error('Set state error: ', error.message);
-  }
-};
-const loadLs = (key) => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
-const removeLs = (key) => {
-  try {
-    localStorage.removeItem(key);
-  } catch (error) {
-    console.error('Remove state error: ', error.message);
-  }
-};
-
-export { saveLs, loadLs, removeLs };
 
 // movieDetails.js
 import { IMG_BASE_URL, IMG_W400 } from './api';
